398fc9f263c72d2abc603a3e754732952a4ce3eb
vinodkumarcvk <54588850+vinodkumarcvk@users.noreply.github.com>
2020-10-12 17:26:19 +0530

    pl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { SubCategoriesComponent } from './sub-categories/sub-categories.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { OrdersComponent } from './orders/orders.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomersComponent } from './customers/customers.component';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { componentFactoryName } from '@angular/compiler';
 
 
 const routes: Routes = [
   { path: 'order', component: OrdersComponent },
-  { path: 'category', component: CategoriesComponent },
+  { path: 'products', component: CategoriesComponent },
   { path: 'subCategory', component: SubCategoriesComponent },
   {path: 'customer', component: CustomersComponent},
-  {path:'login', component: LoginComponent},
-  {path:'signUp', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+{path: 'category', component: CategoriesComponent}
 
 ];
 
